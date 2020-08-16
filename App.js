e3c3daa785649b4487498d96cdebb1e37ad28410
Pallavi G <pallavi.mp88@gmail.com>
2020-08-16 19:39:16 +0530

    handle error messages

--- App.js
+++ App.js
@@ -11,20 +11,26 @@ import {
 const App: () => React$Node = () => {
   const [data, setData] = useState("No Result");
 
-  const makeRequest = async ()=> {
-    try{
-      let response = await fetch("https://randomuser.me/api/");
-      if (response.ok) {
-        let json = await response.json();
-        console.log("api response ",json.results);
-        setData(JSON.stringify(json.results, null, 4));
 
-      } else {
-        alert("HTTP-Error: " + response.status);
+  const makeRequest = async ()=> {
+
+      let response = await fetch("https://randomuser.me/api/");
+      try{
+      if (response.ok) {
+            let json = await response.json();
+            console.log("api response ",json.results);
+            setData(JSON.stringify(json.results, null, 4));
+
+            if(!json.results.length) {
+              alert("Random Data Empty");
+            }
+         } else {
+             alert("HTTP-Error: " + response.status);
+         }
+      } catch (e) {
+          alert("Unexpected error occur" + e);
       }
-    } catch (e) {
-      alert("HTTP-Error: " + e);
-    }
+
   };
 
   return (
